ba43deca1caebe6499a273504c4bba987230be66
samuelleonard63 <samuelleonard63@gmail.com>
2025-05-11 09:18:53 +0000

    moved files to main folder

---
+++ storage/framework/views/f27311abcb9773ee2708be16bfa00b0a.php
@@ -0,0 +1,2 @@
+<div <?php echo e($attributes->class('flex-1')); ?> data-flux-spacer></div>
+<?php /**PATH /home/user/Q-Vault/vendor/livewire/flux/src/../stubs/resources/views/flux/spacer.blade.php ENDPATH**/ ?>
