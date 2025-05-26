0495d9b607f8ae36687c1e50c2792aa9c619853d
samuelleonard63 <samuelleonard63@gmail.com>
2025-05-26 15:09:45 +0000

    stopped to llok for packages for audit and logs and analytics

--- storage/framework/views/f27311abcb9773ee2708be16bfa00b0a.php
+++
@@ -1,2 +0,0 @@
-<div <?php echo e($attributes->class('flex-1')); ?> data-flux-spacer></div>
-<?php /**PATH /home/user/Q-Vault/vendor/livewire/flux/src/../stubs/resources/views/flux/spacer.blade.php ENDPATH**/ ?>
