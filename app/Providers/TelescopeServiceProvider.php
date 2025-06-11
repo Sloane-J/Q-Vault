6edd32756d2c9770cf50825be6007542c37f00f9
Sloane-J <samueldorkeyjr@gmail.com>
2025-06-11 08:15:59 +0000

    defined user entry for telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -56,7 +56,10 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
+            return in_array($user->name, [
+                'Super Admin',
+                'Admin',
+                'Sloane Jnr',
                 //
             ]);
         });
